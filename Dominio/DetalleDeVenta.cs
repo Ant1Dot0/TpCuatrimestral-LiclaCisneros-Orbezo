708305b16986f4e2be1fd3e451c85a66e3050de6
Jose <Jose.orbezo@alumnos.frgp.utn.edu.ar>
2021-11-10 13:21:38 -0300

    Agregue las clases Cliente, Venta, FormaDePago y DetalleDeVenta.

---
+++ Dominio/DetalleDeVenta.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    class DetalleDeVenta
+    {
+        public Venta id { get; set; }
+        public Articulo codigo { get; set; }
+        public int cantidad { get; set; }
+        public decimal precioUnitario { get; set; }
+    }
+}
