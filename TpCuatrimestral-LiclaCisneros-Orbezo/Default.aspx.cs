727e0821f62d942f093debf1f113cf8326857537
Jose <Jose.orbezo@alumnos.frgp.utn.edu.ar>
2021-11-10 10:12:06 -0300

    Agregue las referencias entre capas e agregue contenido en la pagina Default.

--- TpCuatrimestral-LiclaCisneros-Orbezo/Default.aspx.cs
+++ TpCuatrimestral-LiclaCisneros-Orbezo/Default.aspx.cs
@@ -4,14 +4,18 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using Dominio;
+using Negocio;
 
 namespace TpCuatrimestral_LiclaCisneros_Orbezo
 {
     public partial class Default : System.Web.UI.Page
     {
+        public List<Articulo> ListaArticulos { get; set; }
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            ArticuloNegocio reg = new ArticuloNegocio();
+            ListaArticulos = reg.listar();
         }
     }
 }
