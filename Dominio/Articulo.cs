54f6062693611e475dcaecb57771cbdb8b92e237
Jose <Jose.orbezo@alumnos.frgp.utn.edu.ar>
2021-11-24 09:21:26 -0300

    Modifique la clase Articulos.

--- Dominio/Articulo.cs
+++ Dominio/Articulo.cs
@@ -12,22 +12,15 @@ namespace Dominio
         public string codigo { get; set; }
         public string nombre { get; set; }
         public string descripcion { get; set; }
-        public Marca marca { get; set; }
+        //public Marca marca { get; set; }
         //public string descripcionMarca { get; set; }
-        public Categoria categoria { get; set; }
+        //public Categoria categoria { get; set; }
         //public string descripcionCategoria { get; set; }
         public string Url { get; set; }
         public decimal precio { get; set; }
-
-        public override string ToString()
-        {
-            return "id: " + id +
-                    "\r\ncodigo:  " + codigo +
-                    "\r\nnombre:  " + nombre +
-                    "\r\ndescripcion:  " + descripcion +
-                    "\r\nmarca:  " + marca +
-                    "\r\ncategoria:  " + categoria +
-                    "\r\nPrecio:  $" + precio;
-        }
+        public int stock { get; set; }
+        public int limiteStock { get; set; }
+        public string urlImagen { get; set; }
+        public bool activo { get; set; }
     }
 }
